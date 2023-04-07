e24cc97346a48a6b555f62f0cca0bd8bb238dcf5
Ezgi Yıldırım (ETIYA) <ezgi.yildirim@etiya.com>
2023-04-07 12:37:37 +0300

    tamamlandı

---
+++ managementStudentExam/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        StudentManager manager = new StudentManager();
+        Student student1 = new Student(1, "Umut", "Durmuş", 12, 1, "Backend");
+        Student student2 = new Student(2, "Ezgi", "Yıldırım", 152, 2, "Backend");
+        Student student3 = new Student(3, "Umut", "Bal", 20, 3, "Backend");
+        Student student4 = new Student(4, "Umut", "Durmuş", 12, 1, "Backend");
+
+        manager.studentAdd(student1);
+        manager.studentAdd(student2);
+        manager.studentAdd(student3);
+        manager.studentAdd(student4);
+
+        manager.studentRemove(student2);
+
+        manager.allStudentShow();
+
+    }
+}
---
+++ managementStudentExam/src/StudentManager.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentManager {
+
+    List<Student> students = new ArrayList<Student>();
+    public StudentManager(){
+
+    }
+    public void studentAdd(Student student){
+        if(isStudentValid(student.getStudentNo())|| isStudentValid(student.getNationalityId())) {
+            System.out.println("Öğrenci kayıtlı.");
+        } else {
+            students.add(student);
+            System.out.println("Öğrenci eklendi.");
+        }
+    }
+    public void studentRemove(Student student){
+        students.remove(student);
+    }
+    public void allStudentShow(){
+        for(Student student : students) {
+            System.out.println("Öğrenci: " + student.getFirstName() + " " + student.getLastName());
+        }
+    }
+
+    public boolean isStudentValid(int id){
+        for(Student student: students){
+            if(student.getStudentNo() == id || student.getNationalityId() == id){
+                return true;
+            }
+        }
+        return false;
+    }
+}
